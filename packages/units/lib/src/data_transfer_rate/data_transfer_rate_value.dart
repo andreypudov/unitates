270c8c8cc7a6e09a922868babb6fcf8bb08572c0
Andrey Pudov <mail@andreypudov.com>
2023-11-11 14:01:51 +0100

    ~ added draft for data transfer rate units

--- packages/units/lib/src/data_transfer_rate/data_transfer_rate_value.dart
+++ packages/units/lib/src/data_transfer_rate/data_transfer_rate_value.dart
@@ -1,4 +1,17 @@
 import '../../data_transfer_rate_unit.dart';
+import 'bit_per_second.dart';
+import 'gibibit_per_second.dart';
+import 'gigabit_per_second.dart';
+import 'gigabyte_per_second.dart';
+import 'kibibit_per_second.dart';
+import 'kilobit_per_second.dart';
+import 'kilobyte_per_second.dart';
+import 'mebibit_per_second.dart';
+import 'megabit_per_second.dart';
+import 'megabyte_per_second.dart';
+import 'tebibit_per_second.dart';
+import 'terabit_per_second.dart';
+import 'terabyte_per_second.dart';
 
 abstract class DataTransferRateValue {
   final double value;
---
+++ packages/units/lib/src/data_transfer_rate/kilobyte_per_second.dart
@@ -0,0 +1,36 @@
+import '../../data_transfer_rate_unit.dart';
+import 'bit_per_second.dart';
+import 'data_transfer_rate_value.dart';
+import 'gibibit_per_second.dart';
+import 'gigabit_per_second.dart';
+import 'gigabyte_per_second.dart';
+import 'kibibit_per_second.dart';
+import 'kilobit_per_second.dart';
+import 'mebibit_per_second.dart';
+import 'megabit_per_second.dart';
+import 'megabyte_per_second.dart';
+import 'tebibit_per_second.dart';
+import 'terabit_per_second.dart';
+import 'terabyte_per_second.dart';
+
+class KilobytePerSecond extends DataTransferRateValue {
+  KilobytePerSecond(double area) : super(area);
+
+  @override
+  DataTransferRateValue to(DataTransferRateUnit unit) =>
+      switch(unit) {
+        DataTransferRateUnit.bitPerSecond => BitPerSecond(value),
+        DataTransferRateUnit.kilobitPerSecond => KilobitPerSecond(value),
+        DataTransferRateUnit.kilobytePerSecond => KilobytePerSecond(value),
+        DataTransferRateUnit.kibibitPerSecond => KibibitPerSecond(value),
+        DataTransferRateUnit.megabitPerSecond => MegabitPerSecond(value),
+        DataTransferRateUnit.megabytePerSecond => MegabytePerSecond(value),
+        DataTransferRateUnit.mebibitPerSecond => MebibitPerSecond(value),
+        DataTransferRateUnit.gigabitPerSecond => GigabitPerSecond(value),
+        DataTransferRateUnit.gigabytePerSecond => GigabytePerSecond(value),
+        DataTransferRateUnit.gibibitPerSecond => GibibitPerSecond(value),
+        DataTransferRateUnit.terabitPerSecond => TerabitPerSecond(value),
+        DataTransferRateUnit.terabytePerSecond => TerabytePerSecond(value),
+        DataTransferRateUnit.tebibitPerSecond => TebibitPerSecond(value),
+      };
+}
